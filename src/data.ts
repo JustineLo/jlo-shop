b70a70d7353cc91b1c663ff7ab710e6e43af0be9
Justine Lo <justine@ecotree.fr>
2023-07-02 14:06:15 +0200

    projectItem

--- src/data.ts
+++ src/data.ts
@@ -1,9 +1,14 @@
+import project1 from './assets/project1.jpg';
+import project2 from './assets/project2.jpg';
+import project3 from './assets/project3.jpg';
+import project4 from './assets/project4.jpg';
 export interface Project {
     id: number;
     title: string;
     description: string;
     imageUrl: string;
     projectUrl: string;
+    price: number;
 }
 
 export const mockProjects: Project[] = [
@@ -11,71 +16,81 @@ export const mockProjects: Project[] = [
       id: 1,
       title: 'Project 1',
       description: 'This is a description for Project 1',
-      imageUrl: 'http://placeimg.com/640/480/tech/1',
+      imageUrl: project1,
       projectUrl: '#',
+      price: 40
     },
     {
       id: 2,
       title: 'Project 2',
       description: 'This is a description for Project 2',
-      imageUrl: 'http://placeimg.com/640/480/tech/2',
+      imageUrl: project2,
       projectUrl: '#',
+      price: 40
     },
     {
       id: 3,
       title: 'Project 3',
       description: 'This is a description for Project 3',
-      imageUrl: 'http://placeimg.com/640/480/tech/3',
+      imageUrl: project3,
       projectUrl: '#',
+      price: 40
     },
     {
       id: 4,
       title: 'Project 4',
       description: 'This is a description for Project 4',
-      imageUrl: 'http://placeimg.com/640/480/tech/4',
+      imageUrl: project4,
       projectUrl: '#',
+      price: 40
     },
     {
       id: 5,
       title: 'Project 5',
       description: 'This is a description for Project 5',
-      imageUrl: 'http://placeimg.com/640/480/tech/5',
+      imageUrl: project1,
       projectUrl: '#',
+      price: 40
     },
     {
       id: 6,
       title: 'Project 6',
       description: 'This is a description for Project 6',
-      imageUrl: 'http://placeimg.com/640/480/tech/6',
+      imageUrl: project2,
       projectUrl: '#',
+      price: 40
     },
     {
       id: 7,
       title: 'Project 7',
       description: 'This is a description for Project 7',
-      imageUrl: 'http://placeimg.com/640/480/tech/7',
+      imageUrl: project3,
       projectUrl: '#',
+      price: 40
     },
     {
       id: 8,
       title: 'Project 8',
       description: 'This is a description for Project 8',
-      imageUrl: 'http://placeimg.com/640/480/tech/8',
+      imageUrl: project4,
       projectUrl: '#',
+      price: 100
     },
     {
       id: 9,
       title: 'Project 9',
       description: 'This is a description for Project 9',
-      imageUrl: 'http://placeimg.com/640/480/tech/9',
+      imageUrl: project1,
       projectUrl: '#',
+      price: 100
     },
     {
       id: 10,
       title: 'Project 10',
       description: 'This is a description for Project 10',
-      imageUrl: 'http://placeimg.com/640/480/tech/10',
+      imageUrl: project2,
       projectUrl: '#',
+      price: 100
     },
   ];
   
---
+++ src/products/ProjectItem.tsx
@@ -0,0 +1,75 @@
+import React, { useContext } from 'react';
+import { Project } from '../data';
+import { StoreContext } from '../contexts/StoreContext';
+import styled from 'styled-components';
+import Button from '@mui/joy/Button';
+
+const ProjectCard = styled.div`
+  width: 280px;
+  height: 300px;
+  overflow: hidden;
+  border-radius: 20px;
+  box-shadow: 0 2px 18px rgba(0, 0, 0, 0.2);
+  background-color: #FFF2E1;
+`;
+
+const Image = styled.img`
+  height: 40%;
+  width: 100%;
+`;
+
+const Details = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  padding: 20px 20px 0 20px;
+  height: 60%;
+  h2 {
+    margin: 0;
+  }
+`;
+
+const Footer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Price = styled.p`
+  padding-left: 10px;
+  margin: 0;
+  font-weight: bold;
+  font-size: 1rem;
+  align-self: center;
+`;
+
+interface Props {
+  project: Project;
+}
+
+export const ProjectItem: React.FC<Props> = ({ project }) => {
+  const { dispatch } = useContext(StoreContext);
+
+  const addToCart = () => {
+    dispatch({ type: 'ADD_TO_CART', project });
+  };
+
+  return (
+    <ProjectCard>
+      <Image src={project.imageUrl} alt={project.title} />
+      <Details>
+        <h2>{project.title}</h2>
+        <p>{project.description}</p>
+        <Footer>
+          <Price>{project.price}$</Price>
+          <Button
+            color="danger"
+            onClick={addToCart}
+            variant="solid"
+          > Add to Cart</Button>
+        </Footer>
+      </Details>
+
+
+    </ProjectCard>
+  );
+};
