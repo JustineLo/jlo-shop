aabb8a020e58ed126c23e5d6d2a2cd4721a1d3a1
Justine Lo <justine@ecotree.fr>
2023-07-02 17:46:07 +0200

    add stack options filters

--- src/components/FiltersColumn.tsx
+++ src/components/FiltersColumn.tsx
@@ -3,6 +3,9 @@ import Option from '@mui/joy/Option';
 import { styled } from "styled-components";
 import { Slider } from '@mui/joy';
 import { useState } from 'react';
+import Checkbox from '@mui/joy/Checkbox';
+import { stackOptions } from '../data';
+
 
 const Container = styled.div`
     display: flex;
@@ -27,11 +30,24 @@ const PriceFilterContainer = styled.div`
     }
 `;
 
+const StackFilterContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`;
+
+const StackOption = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+`;
+
 function FiltersColumn() {
     return(
         <Container>
             <Sorting />
             <PriceFilter />
+            <StackFilter />
         </Container>
     )
 }
@@ -83,6 +99,25 @@ function PriceFilter() {
             />
         </PriceFilterContainer>
        
+    )
+}
+
+function StackFilter() {
+
+    function handleCheck(event: React.ChangeEvent<HTMLInputElement>) {
+        console.log(event);
+    } 
+
+    return (<StackFilterContainer>
+        <h4>Stack</h4>
+      {stackOptions().map((stack) => {
+        return <StackOption>
+            <Checkbox variant="outlined" value={stack} onChange={(event) => handleCheck(event)} />
+            <p>{stack}</p>
+        </StackOption>
+        }
+      )}
+    </StackFilterContainer>
     )
 }
 
