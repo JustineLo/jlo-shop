ff3ede7b159cb1f9798c0f5e2c4748933ffdf020
Justine Lo <justine@ecotree.fr>
2023-08-11 18:45:42 +0200

    ProjectItem change rightleft to topbottom

--- src/products/ProjectItem.tsx
+++ src/products/ProjectItem.tsx
@@ -1,45 +1,119 @@
-import React, { useContext } from 'react';
-import { Project } from '../data';
-import { StoreContext } from '../contexts/StoreContext';
-import styled from 'styled-components';
-import Button from '@mui/joy/Button';
+import React, { useContext, useState } from "react";
+import { Project } from "../data";
+import { StoreContext } from "../contexts/StoreContext";
+import styled from "styled-components";
+import AddBoxIcon from "@mui/icons-material/AddBox";
+import ProductDetailModal from "../components/ProductDetailModal";
 
 const ProjectCard = styled.div`
+  position: relative;
   width: 280px;
-  height: 300px;
-  overflow: hidden;
-  border-radius: 20px;
-  box-shadow: 0 2px 18px rgba(0, 0, 0, 0.2);
-  background-color: #FFF2E1;
+  height: 350px;
 `;
 
-const Image = styled.img`
-  height: 40%;
+const ImageContainer = styled.div`
+  position: relative;
+  height: 60%;
   width: 100%;
+  overflow: hidden;
+  border-radius: 10px;
+  box-shadow: 0 2px 18px rgba(0, 0, 0, 0.2);
+`;
+
+const Image = styled.div<{ image: string }>`
+  height: 100%;
+  width: 100%;
+  background-image: url(${(props) => props.image});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: transform 0.3s ease-in-out;
+
+  ${ImageContainer}:hover & {
+    transform: scale(1.1);
+  }
+`;
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0.4;
+  transition: 0.5s ease;
+  background-color: #000000;
+
+  ${ProjectCard}:hover & {
+    opacity: 0.1;
+  }
+`;
+
+const SeeDetails = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+  opacity: 0;
+  transition: 0.5s ease;
+  color: white;
+  font-size: 16px;
+  padding: 16px 32px;
+  border: none;
+  cursor: pointer;
+  border-radius: 5px;
+  text-align: center;
+  text-decoration: none;
+
+  ${ProjectCard}:hover & {
+    opacity: 1;
+  }
 `;
 
 const Details = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 20px;
-  padding: 20px 20px 0 20px;
+  gap: 10px;
+  padding: 20px 10px 0 10px;
   height: 60%;
+`;
+
+const Top = styled.div`
+  display: flex; // horizontal arrangement
+  justify-content: space-between; // push items to the edges
+  align-items: center; // vertically center the title and price
+
   h2 {
     margin: 0;
   }
 `;
 
-const Footer = styled.div`
+const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+
+  p {
+    overflow: hidden;
+    text-overflow: ellipsis;
+    flex: 1; // give it room to expand
+    margin-right: 10px; // some spacing between text and button
+  }
+
+  svg {
+    cursor: pointer;
+    padding: 0;
+    &:hover {
+      color: #ff8a8a;
+    }
+  }
 `;
 
 const Price = styled.p`
-  padding-left: 10px;
   margin: 0;
   font-weight: bold;
   font-size: 1rem;
-  align-self: center;
 `;
 
 interface Props {
@@ -48,28 +122,38 @@ interface Props {
 
 export const ProjectItem: React.FC<Props> = ({ project }) => {
   const { dispatch } = useContext(StoreContext);
+  const [showModal, setShowModal] = useState(false);
 
   const addToCart = () => {
-    dispatch({ type: 'ADD_TO_CART', project });
+    dispatch({ type: "ADD_TO_CART", project });
   };
 
   return (
     <ProjectCard>
-      <Image src={project.imageUrl} alt={project.title} />
+      <ImageContainer>
+        <Image image={project.imagesUrl[0]} />
+        <Overlay onClick={() => setShowModal(true)}>
+          <SeeDetails>See Details</SeeDetails>
+        </Overlay>
+      </ImageContainer>
+      <ProductDetailModal
+        project={project}
+        showModal={showModal}
+        onClose={() => setShowModal(false)}
+      />
       <Details>
-        <h2>{project.title}</h2>
-        <p>{project.description}</p>
-        <Footer>
+        <Top>
+          <h2>{project.title}</h2>
           <Price>{project.price}$</Price>
-          <Button
-            color="danger"
+        </Top>
+        <Bottom>
+          <p>{project.excerpt}</p>
+          <AddBoxIcon
+            sx={{ color: "red", fontSize: "35px" }}
             onClick={addToCart}
-            variant="solid"
-          > Add to Cart</Button>
-        </Footer>
+          />
+        </Bottom>
       </Details>
-
-
     </ProjectCard>
   );
 };
