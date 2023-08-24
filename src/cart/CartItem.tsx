f08e2684fc8434761586963f44393b2eb2f390ae
Justine Lo <justine@ecotree.fr>
2023-08-24 14:20:54 +0200

    checkout page

---
+++ src/cart/CartItem.tsx
@@ -0,0 +1,79 @@
+import React, { useContext } from "react";
+import { Project } from "../data";
+import { StoreContext } from "../contexts/StoreContext";
+import styled from "styled-components";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+const CartItemContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  margin: 0;
+  border-radius: 10px;
+  width: 100%;
+  box-shadow: 5px 5px 20px 0px rgba(0, 0, 0, 0.2);
+`;
+
+const ItemDetails = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+`;
+
+const Image = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 10px;
+  margin-right: 15px;
+`;
+const Title = styled.h2`
+  font-size: 1rem;
+  margin: 0;
+  width: 50%;
+`;
+
+const Price = styled.span`
+  font-size: 1rem;
+  margin: 0 15px;
+  width: 20%;
+`;
+
+const RemoveButton = styled.button`
+  padding: 5px 10px;
+  border-radius: 5px;
+  color: #f44336;
+  border: none;
+  cursor: pointer;
+  font-size: 0.8rem;
+  transition: 0.3s;
+  background-color: transparent;
+  &:hover {
+    color: #ff8a8a;
+  }
+`;
+
+interface Props {
+  project: Project;
+}
+
+export const CartItem: React.FC<Props> = ({ project }) => {
+  const { dispatch } = useContext(StoreContext);
+
+  const removeFromCart = () => {
+    dispatch({ type: "REMOVE_FROM_CART", projectId: project.id });
+  };
+
+  return (
+    <CartItemContainer>
+      <ItemDetails>
+        <Image src={project.imagesUrl[0]} alt={project.title} />
+        <Title>{project.title}</Title>
+      </ItemDetails>
+      <Price>${project.price}</Price>
+      <RemoveButton onClick={removeFromCart}>
+        <DeleteIcon />
+      </RemoveButton>
+    </CartItemContainer>
+  );
+};
---
+++ src/views/CheckoutPage.tsx
@@ -0,0 +1,174 @@
+import React, { useContext } from "react";
+import { StoreContext } from "../contexts/StoreContext";
+import styled from "styled-components";
+import { CartItem } from "../cart/CartItem";
+import { useNavigate } from "react-router-dom";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  border-radius: 15px;
+  margin: 60px 0;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 15%;
+  }
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  width: 90%;
+`;
+
+const RecapContainer = styled.div`
+  border-radius: 10px;
+  padding: 20px 30px;
+  width: 80%;
+  height: auto;
+  box-shadow: 5px 5px 10px 0px rgba(0, 0, 0, 0.2);
+
+  @media (min-width: 768px) {
+    width: 20%;
+    height: 100%;
+    padding: 30px 40px;
+  }
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 60px 0;
+  gap: 10px;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    justify-content: space-between;
+  }
+`;
+
+const RecapRow = styled.span`
+  display: flex;
+  width: 100%;
+  margin: 20px 0;
+  justify-content: space-between;
+
+  .bold {
+    font-weight: bold;
+  }
+
+  .title {
+    width: 60%;
+  }
+`;
+
+const CheckoutButton = styled.button`
+  padding: 8px 20px;
+  margin-top: 10px;
+  border-radius: 10px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  cursor: pointer;
+  font-size: 1rem;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+`;
+
+const ClearBasketButton = styled.button`
+  padding: 8px 20px;
+  border-radius: 10px;
+  background-color: #ff0000;
+  color: white;
+  border: none;
+  cursor: pointer;
+  font-size: 1rem;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: #cc0000;
+  }
+`;
+
+const AddMoreButton = styled.button`
+  padding: 8px 20px;
+  border-radius: 10px;
+  background-color: #00bb00;
+  color: white;
+  border: none;
+  cursor: pointer;
+  font-size: 1rem;
+  transition: 0.3s;
+
+  &:hover {
+    background-color: #008800;
+  }
+`;
+
+const CheckoutPage: React.FC = () => {
+  const { state, dispatch } = useContext(StoreContext);
+  const navigate = useNavigate();
+  const total = state.cart.reduce((acc, item) => acc + item.price, 0);
+
+  const handleCheckout = () => {
+    state.cart.forEach((project) => {
+      dispatch({ type: "REMOVE_FROM_CART", projectId: project.id });
+    });
+  };
+
+  const handleAddMore = () => {
+    navigate("/");
+  };
+
+  const handleClearBasket = () => {
+    state.cart.forEach((project) => {
+      dispatch({ type: "REMOVE_FROM_CART", projectId: project.id });
+    });
+  };
+
+  return (
+    <Container>
+      <div>
+        <ItemContainer>
+          {state.cart.map((project) => (
+            <CartItem key={project.id} project={project} />
+          ))}
+        </ItemContainer>
+        <ButtonsContainer>
+          <AddMoreButton onClick={handleAddMore}>
+            Add More Projects
+          </AddMoreButton>
+          <ClearBasketButton onClick={handleClearBasket}>
+            Clear Basket
+          </ClearBasketButton>
+        </ButtonsContainer>
+      </div>
+      <RecapContainer>
+        <RecapRow>
+          <span className="title bold">Subtotal :</span>
+          <span className="bold">${total}</span>
+        </RecapRow>
+        <RecapRow>
+          <span className="title">TVA :</span> <span>${total * 0.1}</span>
+        </RecapRow>
+        <hr color="#c4c4c4" />
+        <RecapRow>
+          <span className="title bold">Total :</span>
+          <span className="bold">${total + total * 0.1}</span>
+        </RecapRow>
+        <CheckoutButton onClick={handleCheckout}>Checkout</CheckoutButton>
+      </RecapContainer>
+    </Container>
+  );
+};
+
+export default CheckoutPage;
