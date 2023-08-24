837907cebbaff0788a1eea0e1f1cd0cebfaec80d
Justine Lo <justine@ecotree.fr>
2023-08-24 14:20:37 +0200

    common header

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import { Cart } from './cart/Cart'
-import { NavBar } from './components/Navbar'
-import HomePage from './views/HomePage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { NavBar } from "./components/Navbar";
+import HomePage from "./views/HomePage";
+import Header from "./components/Header";
+import CheckoutPage from "./views/CheckoutPage";
 
 function App() {
-
   return (
     <BrowserRouter>
       <NavBar />
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/cart" element={<Cart />} />
+        <Route path="/cart" element={<CheckoutPage />} />
       </Routes>
     </BrowserRouter>
-
-  )
+  );
 }
 
-export default App
+export default App;
