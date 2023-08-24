d8786021b02ab144657417744c2b9293e4158e68
Justine Lo <justine@ecotree.fr>
2023-08-24 14:21:13 +0200

    remove header from homepage code

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,19 +1,10 @@
-import styled from "styled-components";
-import Header from "../components/Header";
 import { ProjectsList } from "../products/ProjectsList";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 5rem;
-`;
 
 function HomePage() {
   return (
-    <Container>
-      <Header />
+    <>
       <ProjectsList />
-    </Container>
+    </>
   );
 }
 
